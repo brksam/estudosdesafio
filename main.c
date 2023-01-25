d09e369002145c7a4799c610b6e411cc555896e9
Samuel BRK <samuka.samuca321@gmail.com>
2023-01-25 14:24:28 -0300

    testando novas ferramentas e aprimorando

--- main.c
+++ main.c
@@ -2,21 +2,18 @@
 #include <stdlib.h>
 
 int main() {
-	// Criando variáveis
-		/*NOMA TITULO*/
-	char letraInicial;
-	int idade;
-	float salario;
-	//Iniciando uma variavel
-		/*TITULO (=) ALGO*/
-	letraInicial = 'S';
-	idade = 17;
-	salario = 20000;
-	//saida de dados
-		/*printf por exemplo*/
-	printf("%c \n", letraInicial); // %c caracter
-	printf("%d \n", idade); // %d numero
-	printf("%f", salario); // %f numero quebrado 
-	  
+	float nota1;
+	float nota2;
+	float total; 
+	float media;
+	
+	nota1 = 9;
+	nota2 = 8;
+	total = nota1 + nota2;
+	media = total / 2;
+	
+	printf("total: %f\n", total);
+	printf("media: %f\n", media);
+	
 	return 0;
 }
