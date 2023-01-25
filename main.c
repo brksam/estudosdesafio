f3416cc98eb335ec034bce8b414d499c4cec1876
Samuel BRK <samuka.samuca321@gmail.com>
2023-01-25 14:06:38 -0300

    estudano para fazer o desafio

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	// Criando variáveis
+		/*NOMA TITULO*/
+	char letraInicial;
+	int idade;
+	float salario;
+	//Iniciando uma variavel
+		/*TITULO (=) ALGO*/
+	letraInicial = 'S';
+	idade = 17;
+	salario = 20000;
+	//saida de dados
+		/*printf por exemplo*/
+	printf("%c \n", letraInicial); // %c caracter
+	printf("%d \n", idade); // %d numero
+	printf("%f", salario); // %f numero quebrado 
+	  
+	return 0;
+}
