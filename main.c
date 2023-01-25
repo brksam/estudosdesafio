5fb27d0d30e4559198a55a352ccbfbc32fe207cd
Samuel BRK <samuka.samuca321@gmail.com>
2023-01-25 14:35:51 -0300

    colocando valores

--- main.c
+++ main.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 int main() {
-	float nota1;
-	float nota2;
-	float total; 
-	float media;
+	float desconto;
+	float salario;
+	float inss;
 	
-	nota1 = 9;
-	nota2 = 8;
-	total = nota1 + nota2;
-	media = total / 2;
+	desconto = 5;
+	salario = 1417.44;
+	inss = salario * 0.11;
+	desconto += 2.5;
+	salario -= inss;
 	
-	printf("total: %f\n", total);
-	printf("media: %f\n", media);
+	printf("deconto: %f\n", desconto);
+	printf("salario: %f\n", salario);
 	
 	return 0;
 }
